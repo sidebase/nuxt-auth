405efe390f6ae3652c2e49dcaa7013511845521f
BracketJohn <njonalik@protonmail.com>
2022-12-23 18:41:18 +0100

    fix: do not throw in case edge-case setup attempt fails

--- src/runtime/server/services/nuxtAuthHandler.ts
+++ src/runtime/server/services/nuxtAuthHandler.ts
@@ -215,7 +215,7 @@ export const NuxtAuthHandler = (nuxtAuthOptions?: NextAuthOptions) => {
 export const getServerSession = async (event: H3Event) => {
   if (!preparedAuthHandler) {
     // Edge-case: If no auth-endpoint was called yet, `preparedAuthHandler`-initialization was also not attempted as Nuxt lazily loads endpoints in production-mode. This call gives it a chance to load + initialize the variable. If it fails we still throw. This edge-case has happened to user matijao#7025 on discord.
-    await $fetch(joinURL(useRuntimeConfig().public.auth.basePath, '/session'))
+    await $fetch(joinURL(useRuntimeConfig().public.auth.basePath, '/session')).catch(error => error.data)
     if (!preparedAuthHandler) {
       throw createError({ statusCode: 500, statusMessage: 'Tried to get server session without setting up an endpoint to handle authentication (see https://github.com/sidebase/nuxt-auth#quick-start)' })
     }
