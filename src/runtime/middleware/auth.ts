968498009e5d42e7cf7c29f5e7195f35b11aac8e
BracketJohn <njonalik@protonmail.com>
2023-01-03 09:37:34 +0100

    fix: do not use async composables in auth middleware

--- src/runtime/middleware/auth.ts
+++ src/runtime/middleware/auth.ts
@@ -1,13 +1,14 @@
 import { defineNuxtRouteMiddleware, navigateTo } from '#app'
+import { withQuery } from 'ufo'
 import useSession from '../composables/useSession'
 import { joinPathToApiURL } from '../utils/url'
 
-export default defineNuxtRouteMiddleware(async (to) => {
+export default defineNuxtRouteMiddleware((to) => {
   if (to.meta.auth === false) {
     return
   }
 
-  const { status, signIn } = useSession()
+  const { status } = useSession()
   if (status.value === 'authenticated') {
     return
   }
@@ -15,12 +16,10 @@ export default defineNuxtRouteMiddleware(async (to) => {
   /**
    * Nuxt 3 default behavior is to continue with the route navigation if the middleware does not return a `navigateTo` (see https://nuxt.com/docs/guide/directory-structure/middleware#format).
    *
-   * To avoid content-flashes or brief visibility of protected pages, we thus need to:
-   * 1. `await signIn` so that it will block and trigger it's internal `navigateTo` call. Using `return signIn(...)` does not work eventhough it internally returns `navigateTo`!
-   * 2. fake-return a `navigateTo` here to tell nuxt that the current navigation will be replaced with a new navigation, this avoids the content-flash of the projected page (and should in theory never be triggered due to `signIn` navigating away first!)
-   *
-   * See https://github.com/sidebase/nuxt-auth/issues/100 for a discussion on this topic.
+   * For this reason we build and return a custom `navigateTo` instead of returning `signIn(...)`
    * */
-  await signIn(undefined, { callbackUrl: to.path, redirect: false }, { error: 'SessionRequired' })
-  return navigateTo(joinPathToApiURL('signin'))
+  const url = withQuery(joinPathToApiURL('signin'), {
+    callbackUrl: to.path
+  })
+  return navigateTo('/api/auth/signin')
 })
