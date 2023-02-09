f374a9f646bfefd77397175b57d252e59d6515b4
BracketJohn <njonalik@protonmail.com>
2023-02-09 10:19:25 +0100

    fix: also bump nuxt, nuxt/schema to 3.1.2

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   modules: ['../src/module.ts'],
+  // @ts-expect-error Doesn't detect `auth` config key any more since ca. Nuxt 3.1.2
   auth: {
-    enableGlobalAppMiddleware: true,
-    defaultProvider: undefined
+    enableGlobalAppMiddleware: true
   }
 })
