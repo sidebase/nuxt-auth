8a0a9e6b3f779c3dd6cb725cd04f61a0344ff13a
Jan Hoogeveen <jan.hoogeveen@contextlabs.com>
2023-02-20 14:10:29 +0100

    change typos

--- src/runtime/server/services/nuxtAuthHandler.ts
+++ src/runtime/server/services/nuxtAuthHandler.ts
@@ -1,27 +1,51 @@
-import { getQuery, setCookie, readBody, appendHeader, sendRedirect, eventHandler, parseCookies, createError, isMethod, getMethod, getHeaders } from 'h3'
-import type { H3Event } from 'h3'
+import {
+  getQuery,
+  setCookie,
+  readBody,
+  appendHeader,
+  sendRedirect,
+  eventHandler,
+  parseCookies,
+  createError,
+  isMethod,
+  getMethod,
+  getHeaders,
+} from 'h3';
+import type { H3Event } from 'h3';
 
-import { AuthHandler } from 'next-auth/core'
-import { getToken as nextGetToken } from 'next-auth/jwt'
-import type { RequestInternal } from 'next-auth/core'
-import type { AuthAction, AuthOptions, Session } from 'next-auth'
-import type { GetTokenParams } from 'next-auth/jwt'
+import { AuthHandler } from 'next-auth/core';
+import { getToken as nextGetToken } from 'next-auth/jwt';
+import type { RequestInternal } from 'next-auth/core';
+import type { AuthAction, AuthOptions, Session } from 'next-auth';
+import type { GetTokenParams } from 'next-auth/jwt';
 
-import getURL from 'requrl'
-import { defu } from 'defu'
-import { joinURL } from 'ufo'
-import { isNonEmptyObject } from '../../utils/checkSessionResult'
+import getURL from 'requrl';
+import { defu } from 'defu';
+import { joinURL } from 'ufo';
+import { isNonEmptyObject } from '../../utils/checkSessionResult';
 
-import { useRuntimeConfig } from '#imports'
+import { useRuntimeConfig } from '#imports';
 
-let preparedAuthHandler: ReturnType<typeof eventHandler> | undefined
-let usedSecret: string | undefined
-const SUPPORTED_ACTIONS: AuthAction[] = ['providers', 'session', 'csrf', 'signin', 'signout', 'callback', 'verify-request', 'error', '_log']
+let preparedAuthHandler: ReturnType<typeof eventHandler> | undefined;
+let usedSecret: string | undefined;
+const SUPPORTED_ACTIONS: AuthAction[] = [
+  'providers',
+  'session',
+  'csrf',
+  'signin',
+  'signout',
+  'callback',
+  'verify-request',
+  'error',
+  '_log',
+];
 
 export const ERROR_MESSAGES = {
-  NO_SECRET: 'AUTH_NO_SECRET: No `secret` - this is an error in production, see https://sidebase.io/nuxt-auth/ressources/errors. You can ignore this during development',
-  NO_ORIGIN: 'AUTH_NO_ORIGIN: No `origin` - this is an error in production, see https://sidebase.io/nuxt-auth/ressources/errors. You can ignore this during development'
-}
+  NO_SECRET:
+    'AUTH_NO_SECRET: No `secret` - this is an error in production, see https://sidebase.io/nuxt-auth/resources/errors. You can ignore this during development',
+  NO_ORIGIN:
+    'AUTH_NO_ORIGIN: No `origin` - this is an error in production, see https://sidebase.io/nuxt-auth/resources/errors. You can ignore this during development',
+};
 
 /**
  * Parse a body if the request method is supported, return `undefined` otherwise.
@@ -29,94 +53,111 @@ export const ERROR_MESSAGES = {
 * @param event H3Event event to read body of
  */
 const readBodyForNext = async (event: H3Event) => {
-  let body: any
+  let body: any;
 
-  if (isMethod(event, 'PATCH') || isMethod(event, 'POST') || isMethod(event, 'PUT') || isMethod(event, 'DELETE')) {
-    body = await readBody(event)
+  if (
+    isMethod(event, 'PATCH') ||
+    isMethod(event, 'POST') ||
+    isMethod(event, 'PUT') ||
+    isMethod(event, 'DELETE')
+  ) {
+    body = await readBody(event);
   }
-  return body
-}
+  return body;
+};
 
 /**
  * Get action and optional provider from a request.
  *
  * E.g., with a request like `/api/signin/github` get the action `signin` with the provider `github`
  */
-const parseActionAndProvider = ({ context }: H3Event): { action: AuthAction, providerId: string | undefined } => {
-  const params: string | undefined = context.params._?.split('/')
+const parseActionAndProvider = ({
+  context,
+}: H3Event): { action: AuthAction; providerId: string | undefined } => {
+  const params: string | undefined = context.params._?.split('/');
 
   if (!params || ![1, 2].includes(params.length)) {
-    throw createError({ statusCode: 400, statusMessage: `Invalid path used for auth-endpoint. Supply either one path parameter (e.g., \`/api/auth/session\`) or two (e.g., \`/api/auth/signin/github\` after the base path (in previous examples base path was: \`/api/auth/\`. Received \`${params}\`` })
+    throw createError({
+      statusCode: 400,
+      statusMessage: `Invalid path used for auth-endpoint. Supply either one path parameter (e.g., \`/api/auth/session\`) or two (e.g., \`/api/auth/signin/github\` after the base path (in previous examples base path was: \`/api/auth/\`. Received \`${params}\``,
+    });
   }
 
-  const [unvalidatedAction, providerId] = params
+  const [unvalidatedAction, providerId] = params;
 
   // Get TS to correctly infer the type of `unvalidatedAction`
-  const action = SUPPORTED_ACTIONS.find(action => action === unvalidatedAction)
+  const action = SUPPORTED_ACTIONS.find(
+    (action) => action === unvalidatedAction
+  );
   if (!action) {
-    throw createError({ statusCode: 400, statusMessage: `Called endpoint with unsupported action ${unvalidatedAction!}. Only the following actions are supported: ${SUPPORTED_ACTIONS.join(', ')}` })
+    throw createError({
+      statusCode: 400,
+      statusMessage: `Called endpoint with unsupported action ${unvalidatedAction!}. Only the following actions are supported: ${SUPPORTED_ACTIONS.join(
+        ', '
+      )}`,
+    });
   }
 
-  return { action, providerId }
-}
+  return { action, providerId };
+};
 
 /**
  * Get `origin` and fallback to `x-forwarded-host` or `host` headers if not in production.
  */
 export const getServerOrigin = (event?: H3Event): string => {
   // Prio 1: Environment variable
-  const envOrigin = process.env.AUTH_ORIGIN
+  const envOrigin = process.env.AUTH_ORIGIN;
   if (envOrigin) {
-    return envOrigin
+    return envOrigin;
   }
 
   // Prio 2: Runtime configuration
-  const runtimeConfigOrigin = useRuntimeConfig().auth.origin
+  const runtimeConfigOrigin = useRuntimeConfig().auth.origin;
   if (runtimeConfigOrigin) {
-    return runtimeConfigOrigin
+    return runtimeConfigOrigin;
   }
 
   // Prio 3: Try to infer the origin if we're not in production
   if (event && process.env.NODE_ENV !== 'production') {
-    return getURL(event.node.req)
+    return getURL(event.node.req);
   }
 
-  throw new Error(ERROR_MESSAGES.NO_ORIGIN)
-}
+  throw new Error(ERROR_MESSAGES.NO_ORIGIN);
+};
 
 /** Extract the host from the environment */
 const detectHost = (
   event: H3Event,
-  { trusted, basePath }: { trusted: boolean, basePath: string }
+  { trusted, basePath }: { trusted: boolean; basePath: string }
 ): string | undefined => {
   if (trusted) {
-    const forwardedValue = getURL(event.node.req)
+    const forwardedValue = getURL(event.node.req);
     if (forwardedValue) {
-      return Array.isArray(forwardedValue) ? forwardedValue[0] : forwardedValue
+      return Array.isArray(forwardedValue) ? forwardedValue[0] : forwardedValue;
     }
   }
 
-  let origin
+  let origin;
   try {
-    origin = getServerOrigin(event)
+    origin = getServerOrigin(event);
   } catch (error) {
-    return undefined
+    return undefined;
   }
-  return joinURL(origin, basePath)
-}
+  return joinURL(origin, basePath);
+};
 
 /** Setup the nuxt (next) auth event handler, based on the passed in options */
 export const NuxtAuthHandler = (nuxtAuthOptions?: AuthOptions) => {
-  const isProduction = process.env.NODE_ENV === 'production'
+  const isProduction = process.env.NODE_ENV === 'production';
 
-  usedSecret = nuxtAuthOptions?.secret
+  usedSecret = nuxtAuthOptions?.secret;
   if (!usedSecret) {
     if (isProduction) {
-      throw new Error(ERROR_MESSAGES.NO_SECRET)
+      throw new Error(ERROR_MESSAGES.NO_SECRET);
     } else {
-    // eslint-disable-next-line no-console
-      console.info(ERROR_MESSAGES.NO_SECRET)
-      usedSecret = 'secret'
+      // eslint-disable-next-line no-console
+      console.info(ERROR_MESSAGES.NO_SECRET);
+      usedSecret = 'secret';
     }
   }
 
@@ -124,8 +165,8 @@ export const NuxtAuthHandler = (nuxtAuthOptions?: AuthOptions) => {
     secret: usedSecret,
     logger: undefined,
     providers: [],
-    trustHost: useRuntimeConfig().auth.trustHost
-  })
+    trustHost: useRuntimeConfig().auth.trustHost,
+  });
 
   /**
    * Generate a NextAuth.js internal request object that we can pass into the NextAuth.js
@@ -136,17 +177,22 @@ export const NuxtAuthHandler = (nuxtAuthOptions?: AuthOptions) => {
    *
    * @param event H3Event event to transform into `RequestInternal`
    */
-  const getInternalNextAuthRequestData = async (event: H3Event): Promise<RequestInternal> => {
+  const getInternalNextAuthRequestData = async (
+    event: H3Event
+  ): Promise<RequestInternal> => {
     const nextRequest: Omit<RequestInternal, 'action'> = {
-      host: detectHost(event, { trusted: useRuntimeConfig().auth.trustHost, basePath: useRuntimeConfig().auth.basePath }),
+      host: detectHost(event, {
+        trusted: useRuntimeConfig().auth.trustHost,
+        basePath: useRuntimeConfig().auth.basePath,
+      }),
       body: undefined,
       cookies: parseCookies(event),
       query: undefined,
       headers: getHeaders(event),
       method: getMethod(event),
       providerId: undefined,
-      error: undefined
-    }
+      error: undefined,
+    };
 
     // Setting `event.context.checkSessionOnNonAuthRequest = true` allows callers of `authHandler`.
     // We can use this to check session status on the server-side.
@@ -157,19 +203,22 @@ export const NuxtAuthHandler = (nuxtAuthOptions?: AuthOptions) => {
       return {
         ...nextRequest,
         method: 'GET',
-        action: 'session'
-      }
+        action: 'session',
+      };
     }
 
     // Figure out what action, providerId (optional) and error (optional) of the NextAuth.js lib is targeted
-    const query = getQuery(event)
-    const { action, providerId } = parseActionAndProvider(event)
-    const error = query.error
+    const query = getQuery(event);
+    const { action, providerId } = parseActionAndProvider(event);
+    const error = query.error;
     if (Array.isArray(error)) {
-      throw createError({ statusCode: 400, statusMessage: 'Error query parameter can only appear once' })
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'Error query parameter can only appear once',
+      });
     }
 
-    const body = await readBodyForNext(event)
+    const body = await readBodyForNext(event);
 
     return {
       ...nextRequest,
@@ -177,32 +226,36 @@ export const NuxtAuthHandler = (nuxtAuthOptions?: AuthOptions) => {
       query,
       action,
       providerId,
-      error: error || undefined
-    }
-  }
+      error: error || undefined,
+    };
+  };
 
   const handler = eventHandler(async (event: H3Event) => {
-    const { res } = event.node
+    const { res } = event.node;
 
     // 1. Assemble and perform request to the NextAuth.js auth handler
-    const nextRequest = await getInternalNextAuthRequestData(event)
+    const nextRequest = await getInternalNextAuthRequestData(event);
 
     const nextResult = await AuthHandler({
       req: nextRequest,
-      options
-    })
+      options,
+    });
 
     // 2. Set response status, headers, cookies
     if (nextResult.status) {
-      res.statusCode = nextResult.status
+      res.statusCode = nextResult.status;
     }
-    nextResult.cookies?.forEach(cookie => setCookie(event, cookie.name, cookie.value, cookie.options))
-    nextResult.headers?.forEach(header => appendHeader(event, header.key, header.value))
+    nextResult.cookies?.forEach((cookie) =>
+      setCookie(event, cookie.name, cookie.value, cookie.options)
+    );
+    nextResult.headers?.forEach((header) =>
+      appendHeader(event, header.key, header.value)
+    );
 
     // 3. Return either:
     // 3.1 the body directly if no redirect is set:
     if (!nextResult.redirect) {
-      return nextResult.body
+      return nextResult.body;
     }
     // 3.2 a json-object with a redirect url if `json: true` is set by client:
     //      ```
@@ -214,47 +267,55 @@ export const NuxtAuthHandler = (nuxtAuthOptions?: AuthOptions) => {
     //      returning an HTTP response with a redirect for non-JavaScript clients).
     //      ```
     if (nextRequest.body?.json) {
-      return { url: nextResult.redirect }
+      return { url: nextResult.redirect };
     }
 
     // 3.3 via a redirect:
-    return sendRedirect(event, nextResult.redirect)
-  })
+    return sendRedirect(event, nextResult.redirect);
+  });
 
   // Save handler so that it can be used in other places
   if (preparedAuthHandler) {
     // eslint-disable-next-line no-console
-    console.warn('You setup the auth handler for a second time - this is likely undesired. Make sure that you only call `NuxtAuthHandler( ... )` once')
+    console.warn(
+      'You setup the auth handler for a second time - this is likely undesired. Make sure that you only call `NuxtAuthHandler( ... )` once'
+    );
   }
-  preparedAuthHandler = handler
-  return handler
-}
+  preparedAuthHandler = handler;
+  return handler;
+};
 
 export const getServerSession = async (event: H3Event) => {
-  const authBasePath = useRuntimeConfig().public.auth.basePath
+  const authBasePath = useRuntimeConfig().public.auth.basePath;
   // avoid running auth middleware on auth middleware (see #186)
   if (event.path && event.path.startsWith(authBasePath)) {
-    return null
+    return null;
   }
   if (!preparedAuthHandler) {
     // Edge-case: If no auth-endpoint was called yet, `preparedAuthHandler`-initialization was also not attempted as Nuxt lazily loads endpoints in production-mode. This call gives it a chance to load + initialize the variable. If it fails we still throw. This edge-case has happened to user matijao#7025 on discord.
-    await $fetch(joinURL(authBasePath, '/session')).catch(error => error.data)
+    await $fetch(joinURL(authBasePath, '/session')).catch(
+      (error) => error.data
+    );
     if (!preparedAuthHandler) {
-      throw createError({ statusCode: 500, statusMessage: 'Tried to get server session without setting up an endpoint to handle authentication (see https://github.com/sidebase/nuxt-auth#quick-start)' })
+      throw createError({
+        statusCode: 500,
+        statusMessage:
+          'Tried to get server session without setting up an endpoint to handle authentication (see https://github.com/sidebase/nuxt-auth#quick-start)',
+      });
     }
   }
 
   // Run a session check on the event with an arbitrary target endpoint
-  event.context.checkSessionOnNonAuthRequest = true
-  const session = await preparedAuthHandler(event)
-  delete event.context.checkSessionOnNonAuthRequest
+  event.context.checkSessionOnNonAuthRequest = true;
+  const session = await preparedAuthHandler(event);
+  delete event.context.checkSessionOnNonAuthRequest;
 
   if (isNonEmptyObject(session)) {
-    return session as Session
+    return session as Session;
   }
 
-  return null
-}
+  return null;
+};
 
 /**
  * Get the decoded JWT token either from cookies or header (both are attempted).
@@ -263,14 +324,20 @@ export const getServerSession = async (event: H3Event) => {
  *
  * @param eventAndOptions Omit<GetTokenParams, 'req'> & { event: H3Event } The event to get the cookie or authorization header from that contains the JWT Token and options you want to alter token getting behavior.
  */
-export const getToken = ({ event, secureCookie, secret, ...rest }: Omit<GetTokenParams, 'req'> & { event: H3Event }) => nextGetToken({
-  // @ts-expect-error As our request is not a real next-auth request, we pass down only what's required for the method, as per code from https://github.com/nextauthjs/next-auth/blob/8387c78e3fef13350d8a8c6102caeeb05c70a650/packages/next-auth/src/jwt/index.ts#L68
-  req: {
-    cookies: parseCookies(event),
-    headers: getHeaders(event)
-  },
-  // see https://github.com/nextauthjs/next-auth/blob/8387c78e3fef13350d8a8c6102caeeb05c70a650/packages/next-auth/src/jwt/index.ts#L73
-  secureCookie: secureCookie || getServerOrigin(event).startsWith('https://'),
-  secret: secret || usedSecret,
+export const getToken = ({
+  event,
+  secureCookie,
+  secret,
   ...rest
-})
+}: Omit<GetTokenParams, 'req'> & { event: H3Event }) =>
+  nextGetToken({
+    // @ts-expect-error As our request is not a real next-auth request, we pass down only what's required for the method, as per code from https://github.com/nextauthjs/next-auth/blob/8387c78e3fef13350d8a8c6102caeeb05c70a650/packages/next-auth/src/jwt/index.ts#L68
+    req: {
+      cookies: parseCookies(event),
+      headers: getHeaders(event),
+    },
+    // see https://github.com/nextauthjs/next-auth/blob/8387c78e3fef13350d8a8c6102caeeb05c70a650/packages/next-auth/src/jwt/index.ts#L73
+    secureCookie: secureCookie || getServerOrigin(event).startsWith('https://'),
+    secret: secret || usedSecret,
+    ...rest,
+  });
