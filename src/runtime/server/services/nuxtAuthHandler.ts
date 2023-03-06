795e520b6dcfa3d9c0066f123eba62c903554ec8
Jan Hoogeveen <github@janhoogeveen.eu>
2023-03-06 16:16:54 +0100

    docs: Fix spelling of ressources to resources (#251)

--- src/runtime/server/services/nuxtAuthHandler.ts
+++ src/runtime/server/services/nuxtAuthHandler.ts
@@ -19,8 +19,8 @@ let usedSecret: string | undefined
 const SUPPORTED_ACTIONS: AuthAction[] = ['providers', 'session', 'csrf', 'signin', 'signout', 'callback', 'verify-request', 'error', '_log']
 
 export const ERROR_MESSAGES = {
-  NO_SECRET: 'AUTH_NO_SECRET: No `secret` - this is an error in production, see https://sidebase.io/nuxt-auth/ressources/errors. You can ignore this during development',
-  NO_ORIGIN: 'AUTH_NO_ORIGIN: No `origin` - this is an error in production, see https://sidebase.io/nuxt-auth/ressources/errors. You can ignore this during development'
+  NO_SECRET: 'AUTH_NO_SECRET: No `secret` - this is an error in production, see https://sidebase.io/nuxt-auth/resources/errors. You can ignore this during development',
+  NO_ORIGIN: 'AUTH_NO_ORIGIN: No `origin` - this is an error in production, see https://sidebase.io/nuxt-auth/resources/errors. You can ignore this during development'
 }
 
 /**
