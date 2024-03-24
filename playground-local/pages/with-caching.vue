9843c3092ca7b347895305e3a86e5344e278f66a
kylesmith090502 <me@yskkyle.com>
2024-03-24 19:22:44 -0400

    fix(playground): On SWR pages, display time.

--- playground-local/pages/with-caching.vue
+++ playground-local/pages/with-caching.vue
@@ -1,31 +1,16 @@
 <script setup lang="ts">
-import { computed, definePageMeta, onMounted, ref } from '#imports'
+import { definePageMeta, ref, useState } from '#imports'
 
-const cachedAt = ref(new Date())
-const enteredAt = ref<Date | undefined>(undefined)
+const clientRenderTime = ref<Date>(new Date())
 
-const cachedAtTime = computed(() => cachedAt.value.getTime())
-const enteredAtTime = computed(() => (enteredAt.value?.getTime() ?? 0))
-
-const relativeTimeFormat = ref(new Intl.RelativeTimeFormat('en'))
-
-onMounted(() => {
-  enteredAt.value = new Date()
-})
+const serverRenderTime = useState('server-render-date', () => new Date())
 
 definePageMeta({ auth: false })
 </script>
 
 <template>
   <div>
-    <p v-if="cachedAtTime < enteredAtTime - 5000">
-      This page was cached
-      {{ relativeTimeFormat.format((cachedAtTime / 60000) - (enteredAtTime / 60000), 'minutes') }}.
-    </p>
-    <p v-else>
-      This page was not cached.
-    </p>
-    <p>Cached At: {{ cachedAt?.toISOString() }}.</p>
-    <p>Created At: {{ enteredAt?.toISOString() }}.</p>
+    <p>Server Render Time: {{ serverRenderTime?.toISOString() }}</p>
+    <p>Client Render Time: {{ clientRenderTime?.toISOString() }}</p>
   </div>
 </template>
