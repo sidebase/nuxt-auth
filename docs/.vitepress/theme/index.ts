3f344568a3c21f76d7f59854311d3e346a81e3f8
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-07-04 22:25:40 +0200

    added github star counter

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import GithubButton from 'vue-github-button'
 
 import './style.css'
 
@@ -9,7 +10,11 @@ export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'home-hero-info-before': h(GithubButton, {
+        href: 'https://github.com/sidebase/nuxt-auth',
+        dataShowCount: true,
+        dataSize: 'large',
+      }, 'Star'),
     })
   },
 } satisfies Theme
