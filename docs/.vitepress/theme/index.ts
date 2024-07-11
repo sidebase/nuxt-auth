590746c40df36f3088f2fb53f14cb2758ffc010f
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-07-11 14:23:53 +0200

    Added an annoucement banner

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,13 +8,25 @@ import './style.css'
 
 // Custom Components
 import GithubStarsButton from './components/GithubStarsButton.vue'
+import Banner from './components/Banner.vue'
+
+// Configuration
+const bannerConfig = {
+  // Leave text empty to disable the banner
+  text: '✨ NuxtAuth v0.8.0 has been released! ✨',
+  button: {
+    href: 'https://github.com/sidebase/nuxt-auth/releases/tag/0.8.0',
+    text: 'View release notes'
+  }
+}
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'nav-bar-content-after': h(GithubStarsButton, { owner: 'sidebase', repo: 'nuxt-auth' })
+      'nav-bar-content-after': h(GithubStarsButton, { owner: 'sidebase', repo: 'nuxt-auth' }),
+      'home-hero-before': h(Banner, bannerConfig)
     })
   },
 } satisfies Theme
