c15f6ce150b676ac20fb6f1fffb2dea80f0f3f92
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-07-11 13:53:53 +0200

    added Github Star Button

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,13 +3,18 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 
+// Styles
 import './style.css'
+
+// Custom Components
+import GithubStarsButton from './components/GithubStarsButton.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'nav-bar-content-after': h(GithubStarsButton, { owner: 'sidebase', repo: 'nuxt-auth' })
     })
   },
 } satisfies Theme
