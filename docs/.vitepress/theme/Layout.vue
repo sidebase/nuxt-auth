1eb118e79d5d15f30e0269eba8b6ee308abf1dba
Zoey Kaiser <zoeykaiser8@gmail.com>
2024-08-02 09:23:28 +0200

    Improved Layout to not remove components on re-render

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import GithubStarsButton from './components/GithubStarsButton.vue'
+import Banner from './components/Banner.vue'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #nav-bar-content-after>
+      <GithubStarsButton owner="sidebase" repo="nuxt-auth" />
+    </template>
+
+    <template #home-hero-before>
+      <Banner
+        text="✨ NuxtAuth v0.8.0 has been released! ✨"
+        :button="{
+          href: 'https://github.com/sidebase/nuxt-auth/releases/tag/0.8.0',
+          text: 'View release notes'
+        }"
+      />
+    </template>
+  </Layout>
+</template>
