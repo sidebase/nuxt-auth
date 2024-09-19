5276a97997e4d1d5d3f0b76c0b064c0ab86e92cd
andychukse <andychukse@users.noreply.github.com>
2024-09-19 12:27:51 +0100

    enh(#861): Redirect back to requested page after authentication (#870)
    * merge with main
    
    * added check for presence of addDefaultCallbackUrl in globalAppMiddleware
    
    * Update src/runtime/composables/local/useAuth.ts
    
    * Update useAuth.ts
    
    ---------
    
    Co-authored-by: Zoey <zoeykaiser8@gmail.com>
    Co-authored-by: Marsel Shayhin <18054980+phoenix-ru@users.noreply.github.com>

--- src/runtime/middleware/auth.ts
+++ src/runtime/middleware/auth.ts
@@ -105,6 +105,19 @@ export default defineNuxtRouteMiddleware((to) => {
     return navigateTo(metaAuth.navigateUnauthenticatedTo)
   }
   else {
+    if (typeof globalAppMiddleware === 'object' && globalAppMiddleware.addDefaultCallbackUrl) {
+      let redirectUrl: string = to.fullPath
+      if (typeof globalAppMiddleware.addDefaultCallbackUrl === 'string') {
+        redirectUrl = globalAppMiddleware.addDefaultCallbackUrl
+      }
+
+      return navigateTo({
+        path: authConfig.provider.pages.login,
+        query: {
+          redirect: redirectUrl
+        }
+      })
+    }
     return navigateTo(authConfig.provider.pages.login)
   }
 })
